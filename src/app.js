434b8e2b4b3e8c55d204eb28245e77805c0312eb
jhcamara <jhcamara@gmail.com>
2022-10-21 19:05:11 +0000

    Final result

--- src/app.js
+++ src/app.js
@@ -8,4 +8,32 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+
+  let set = ["hearts", "diamonds", "spades", "clubs"];
+  let card = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+
+  function shuffle(item) {
+    return item[Math.floor(Math.random() * item.length)];
+  }
+  let newSet = shuffle(set);
+  let newCard = shuffle(card);
+
+  console.log(newSet);
+  console.log(newCard);
+
+  function createCard() {
+    let element = document.querySelectorAll("#top, #bottom");
+
+    element.forEach(elem => elem.classList.add(`${newSet}`));
+    let element2 = document.querySelector("#number");
+    element2.innerHTML = `${newCard}`;
+  }
+
+  createCard();
+
+  // document.querySelector("#btn");
+
+  setTimeout(function() {
+    window.location.reload(1);
+  }, 10000);
 };
